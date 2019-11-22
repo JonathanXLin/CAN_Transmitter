7550de966144cd972d2c05e08d52cafbe55a0604
Jonathan Lin <jlin1999@gmail.com>
2019-11-22 13:44:48 -0500

    added 500kbps mode

--- code/CAN_Transmitter/can_cyclic.cpp
+++ code/CAN_Transmitter/can_cyclic.cpp
@@ -1,6 +1,6 @@
 #include "can_cyclic.hpp"
 
-void CAN_initialize(MCP_CAN myCan)
+void CAN_initialize(MCP_CAN myCan_250KBPS, MCP_CAN myCan_500KBPS)
 {
   pinMode(led_pwr, OUTPUT);
   pinMode(led_tx, OUTPUT);
@@ -11,13 +11,28 @@ void CAN_initialize(MCP_CAN myCan)
   Serial.begin(9600);
 
   // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
-  if(myCan.begin(MCP_ANY, CAN_250KBPS, MCP_16MHZ) == CAN_OK) 
-    Serial.println("MCP2515 Initialized Successfully!");
+  if(myCan_250KBPS.begin(MCP_ANY, CAN_250KBPS, MCP_16MHZ) == CAN_OK) 
+    Serial.println("MCP2515 250KBPS Initialized Successfully!");
   else 
   {
     while (1)
     {
-      Serial.println("Error Initializing MCP2515...");
+      Serial.println("Error Initializing MCP2515 250KBPS...");
+      
+      digitalWrite(led_pwr, HIGH);
+      delay(300);
+      digitalWrite(led_pwr, LOW);
+      delay(300);
+    }
+  }
+  
+  if(myCan_500KBPS.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK) 
+    Serial.println("MCP2515 500KBPS Initialized Successfully!");
+  else 
+  {
+    while (1)
+    {
+      Serial.println("Error Initializing MCP2515 500KBPS...");
       
       digitalWrite(led_pwr, HIGH);
       delay(300);
@@ -26,7 +41,8 @@ void CAN_initialize(MCP_CAN myCan)
     }
   }
 
-  myCan.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
+  myCan_250KBPS.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
+  myCan_500KBPS.setMode(MCP_NORMAL);
 
   digitalWrite(led_pwr, HIGH);
 }
