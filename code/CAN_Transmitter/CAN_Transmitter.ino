24123d6f6074fe4ea30dbf043e9d4036d38a9f73
Jonathan Lin <jlin1999@gmail.com>
2019-11-24 15:40:16 -0500

    Final update for baud rate switch

--- code/CAN_Transmitter/CAN_Transmitter.ino
+++ code/CAN_Transmitter/CAN_Transmitter.ino
@@ -1,7 +1,6 @@
 #include "can_cyclic.hpp"
 
-MCP_CAN myCan_250KBPS(10);     // Set CS to pin 10
-MCP_CAN myCan_500KBPS(10);     // Set CS to pin 10
+MCP_CAN myCan(10);     // Set CS to pin 10
 
 // ============== ADD AND EDIT CAN MESSAGE PARAMETERS ACCORDING TO FORMAT BELOW ======================
 // byte your_data_name[8] = {your_data};
@@ -12,18 +11,17 @@ CAN_message_cyclic CCVS1(0x18FEF100, 8, CCVS1_data, 100);
 
 byte EBC2_data[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 CAN_message_cyclic EBC2(0x18FEBF0B, 8, EBC2_data, 100);
-
 // ===================================================================================================
 
 void setup()
 {
   // Initializes CAN and LEDs
-  CAN_initialize(myCan_250KBPS, myCan_500KBPS);
+  CAN_initialize(myCan);
 }
 
 void loop()
 {
   // One line for each CAN message
-  CCVS1.send_CAN(myCan_250KBPS, myCan_500KBPS);
-  EBC2.send_CAN(myCan_250KBPS, myCan_500KBPS);
+  CCVS1.send_CAN(myCan);
+  EBC2.send_CAN(myCan);
 }
--- code/CAN_Transmitter/can_cyclic.hpp
+++ code/CAN_Transmitter/can_cyclic.hpp
@@ -19,68 +19,10 @@ class CAN_message_cyclic
     unsigned long millis_next; // when millis exceeds this variable, send message again and increment it by the period
     unsigned long led_millis_next; // when LED turns on, this variable is made to be 10ms greater than millis(), and turns off when millis() exceeds it
     bool led_flag; // set to true when LED is on, reset when LED is off
+    int pin_baud_select_prev; // previous state of pin baud select
   public:
-    CAN_message_cyclic(long can_id, int can_dlc, byte *can_data, int can_period)
-    {
-      id = can_id;
-      dlc = can_dlc;
-
-      for (int i=0; i<8; i++)
-      {
-        data[i] = can_data[i];
-      }
-
-      period = can_period;
-      millis_next = millis() + period;
-
-      led_millis_next = 999999999999; // Initialize to large so LED functions will not run when no CAN message has transmitted
-      led_flag = false;
-    }
-
-    void send_CAN(MCP_CAN myCan_250KBPS, MCP_CAN myCan_500KBPS)
-    {
-      if (millis() > millis_next)
-      {
-        // send data:  ID = 0x100, Standard CAN Frame, Data length = 8 bytes, 'data' = array of data bytes to send
-        byte sndStat = 0;
-        
-        if (digitalRead(pin_baud_select) == HIGH)
-          sndStat = myCan_250KBPS.sendMsgBuf(id, CAN_ID_29_BIT, dlc, data);
-        else
-          sndStat = myCan_500KBPS.sendMsgBuf(id, CAN_ID_29_BIT, dlc, data);
-        
-        if(sndStat == CAN_OK)
-        {
-          //Serial.print("SENT AND RECEIVED, message id: ");
-          //Serial.print(id, HEX);
-          //Serial.println();
-        } 
-        else 
-        {
-          //Serial.print("ERROR SENDING MESSAGE, message id: ");
-          //Serial.print(id, HEX);
-          //Serial.println();
-        }
-
-        millis_next = millis() + period; // Update millis_next
-
-        if (!led_flag)
-        {
-          digitalWrite(led_tx, HIGH);
-          led_flag = true;
-          Serial.println("1");
-        }
-
-        led_millis_next = millis() + 50; // Update led_millis_next
-      }
-
-      if (millis() > led_millis_next && led_flag)
-      {
-        digitalWrite(led_tx, LOW);
-        led_flag = false;
-        Serial.println("0");
-      }
-    }
+    CAN_message_cyclic(long can_id, int can_dlc, byte *can_data, int can_period);
+    void send_CAN(MCP_CAN myCan);
 };
 
-void CAN_initialize(MCP_CAN myCan_250KBPS, MCP_CAN myCan_500KBPS);
+void CAN_initialize(MCP_CAN myCan);
